7d5e0244dccc32787ed42a6ebe0415e43c6c448b
kimothokamau <kimothokamau@gmail.com>
2021-09-08 01:43:05 -0600

    Restructure folders, redux store, book actions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Books from './redux/books/books';
-import New from './redux/books/new';
-import Categories from './redux/categories/categories';
+import New from './components/books/new';
+import Categories from './components/categories/categories';
 import Nav from './components/nav';
 
 function App() {
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import Book from './book';
-import books from '../../components/fakeBooks';
+import { v4 as uuidv4 } from 'uuid';
 
-const Books = () => (
-  <ul>
-    {
-        books.map((book) => {
-          const { id, title, author } = book;
-          return (
-            <Book key={id} title={title} author={author} />
-          );
-        })
-      }
-  </ul>
-);
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-export default Books;
+const initialState = [];
+
+const addBookAction = (title, author) => ({
+  type: ADD_BOOK,
+  payload: {
+    title,
+    author,
+    id: uuidv4(),
+  },
+});
+
+const removeBookAction = (id) => ({
+  type: REMOVE_BOOK,
+  payload: {
+    id,
+  },
+});
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
+
+export { addBookAction, removeBookAction, booksReducer };
